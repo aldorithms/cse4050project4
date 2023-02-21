7be53ab6c5e7683f73a6eacc706a19124fa75561
Aldo A. Avalos <99375833+aldorithms@users.noreply.github.com>
2023-02-21 00:29:10 -0800

    Add files via upload

---
+++ gettingStarted.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/main.css';
+
+import Example from './components/example/Example';
+
+const root = ReactDOM.createRoot(document.getElementById('reactapp'));
+
+root.render(<Example />);
---
+++ p2.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import CSECourses from './components/courses/CSECourses';
+
+const root = ReactDOM.createRoot(document.getElementById('reactapp'));
+
+root.render(<CSECourses />);
